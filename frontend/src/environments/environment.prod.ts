1fd4c873817ceaf158fa5724d0ae4788b26cc247
Gonzalo <gonzasil01@gmail.com>
2026-02-07 18:10:16 -0300

    Actualizar URL del backend para producción

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://vencimientos-app-backend.onrender.com' // Cambia esto por la URL real que te asigne Render para tu Web Service
+    apiUrl: 'https://venceapp.onrender.com'
 };
